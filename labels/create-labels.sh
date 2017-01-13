6b8207a005e80a0523059fdfb37eb88448421e6e
TC <tobalsemail@gmail.com>
2017-01-13 16:05:49 -0700

    Create label scripts

---
+++ labels/create-labels.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Creates the labels defined in labels.json in the repositories defined in repos.json
+#  author: {{TC}}
+#  created: {{1/13/2017}}
+#  updated:
+#  required permissions: none
+#  required env vars: GITHUB_TOKEN - should be set to a personal access token
+# Usage:
+#  bash create-labels.sh
+
+set -o nounset # Abort if you reference an undefined variable
+set -o errexit # Abort if a command returns non-zero exit code
+set -o pipefail # Abort if a pipeline includes non-zero exit codes (requires Bash v3.x)
+
+if [ -z $GITHUB_TOKEN ]; then
+  echo "GITHUB_TOKEN must be set"
+  exit 1
+fi
+
+# OWNER="100health"
+# REPO="redox-github-management"
+LABELS=$(cat labels.json)
+NUM_LABELS=$(echo $LABELS | jq '. | length')
+REPOS=$(cat ../repos.json)
+NUM_REPOS=$(echo $REPOS | jq '. | length')
+
+source ./label-functions.sh
+
+labelIdx=0
+while [[ $labelIdx -lt $NUM_LABELS ]]
+do
+  NAME=$(echo $LABELS | jq -r --arg labelIdx $labelIdx '.[$labelIdx|tonumber].name')
+  COLOR=$(echo $LABELS | jq -r --arg labelIdx $labelIdx '.[$labelIdx|tonumber].color')
+
+  echo "Creating / Updating label $NAME with color $COLOR"
+
+  repoIdx=0
+  while [[ $repoIdx -lt $NUM_REPOS ]]
+  do
+    REPO=$(echo $REPOS | jq -r --arg repoIdx $repoIdx '.[$repoIdx|tonumber].name')
+    OWNER=$(echo $REPOS | jq -r --arg repoIdx $repoIdx '.[$repoIdx|tonumber].owner')
+
+    createLabel -n="$NAME" -c="$COLOR" -o="$OWNER" -r="$REPO"
+
+    ((repoIdx = repoIdx + 1))
+  done
+
+  ((labelIdx = labelIdx + 1))
+  echo
+done
---
+++ labels/label-functions.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+set -o nounset # quit if we reference an unset variable
+set -o errexit # quit if a command returns non-zero exit code (a.k.a. set -e)
+set -o pipefail # Abort if a pipeline includes non-zero exit codes (requires Bash v3.x)
+
+createLabel () {
+  # Parsing out the arguments for the function
+  # -n - the name of the label
+  # -c - the color of the label
+  # -o - the owner of the repo
+  # -r - the repo
+  for i in "$@"
+  do
+  case $i in
+    -n=*|--name=*)
+    local _NAME="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--color=*)
+    local _COLOR="${i#*=}"
+    shift # past argument=value
+    ;;
+    -o=*|--owner=*)
+    local _OWNER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -r=*|--repo=*)
+    local _REPO="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    ;;
+  esac
+  done
+
+  if [[ -z "${_NAME:-}" ]]; then
+    echo "_NAME is required. Use option -n"
+    return 1
+  fi
+
+  if [[ -z "${_COLOR:-}" ]]; then
+    echo "_COLOR is required. Use option -c"
+    return 1
+  fi
+
+  if [[ -z "${_OWNER:-}" ]]; then
+    echo "_OWNER is required. Use option -o"
+    return 1
+  fi
+
+  if [[ -z "${_REPO:-}" ]]; then
+    echo "_REPO is required. Use option -r"
+    return 1
+  fi
+
+  # Create the label
+  local CURL_OUTPUT=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST "https://api.github.com/repos/$_OWNER/$_REPO/labels" -d "{\"name\":\"$_NAME\", \"color\":\"$_COLOR\"}")
+  local HAS_ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors // empty')
+
+  # Check if there was an error
+  if [ ! -z "$HAS_ERROR" ]; then
+    local ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors[0].code')
+
+    # If the label already exists
+    if [ "$ERROR" == "already_exists" ]; then
+      # We update
+      # echo "Label $_NAME already exists. Updating..."
+      updateLabel -n="$_NAME" -c="$_COLOR" -o="$_OWNER" -r="$_REPO"
+    else
+      echo "Unknown error: $ERROR"
+      echo "Output from curl: "
+      echo "$CURL_OUTPUT"
+      return 1
+    fi
+  else
+    echo "Created label $_NAME with color $_COLOR in $_OWNER/$_REPO."
+  fi
+}
+
+updateLabel () {
+  # Parsing out the arguments for the function
+  # -n - the name of the label
+  # -c - the color of the label
+  # -o - the owner of the repo
+  # -r - the repo
+  for i in "$@"
+  do
+  case $i in
+    -n=*|--name=*)
+    local _NAME="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--color=*)
+    local _COLOR="${i#*=}"
+    shift # past argument=value
+    ;;
+    -o=*|--owner=*)
+    local _OWNER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -r=*|--repo=*)
+    local _REPO="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    ;;
+  esac
+  done
+
+  if [[ -z "${_NAME:-}" ]]; then
+    echo "_NAME is required. Use option -n"
+    return 1
+  fi
+
+  if [[ -z "${_COLOR:-}" ]]; then
+    echo "_COLOR is required. Use option -c"
+    return 1
+  fi
+
+  if [[ -z "${_OWNER:-}" ]]; then
+    echo "_OWNER is required. Use option -o"
+    return 1
+  fi
+
+  if [[ -z "${_REPO:-}" ]]; then
+    echo "_REPO is required. Use option -r"
+    return 1
+  fi
+
+  local CURL_OUTPUT=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -X PATCH "https://api.github.com/repos/$_OWNER/$_REPO/labels/${_NAME// /%20}" -d "{\"name\":\"$_NAME\", \"color\":\"$_COLOR\"}")
+  local HAS_ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors // empty')
+
+  # Check for error
+  if [ ! -z "$HAS_ERROR" ]; then
+    local ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors[0].code')
+    echo "Unknown error: $ERROR"
+    echo "Output from curl: "
+    echo "$CURL_OUTPUT"
+    return 1
+  else
+    echo "Updated label $_NAME with color $_COLOR in $_OWNER/$_REPO."
+  fi
+
+}
